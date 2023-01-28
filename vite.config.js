a1a0c9ae4277646040f0bcc2eeb8e09429484e28
Felipe <felipecartes.u@gmail.com>
2023-01-28 05:52:25 -0300

    SDASD

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { extname } from 'path';
 
 // https://vitejs.dev/config/
 
 
 
 export default defineConfig({
-  plugins: [react(),
-    babel({
-      babelConfig: {
-          babelrc: false,
-          configFile: false,
-          plugins: ['@babel/plugin-proposal-decorators'],
-          
-          // uses the jsx loader for .jsx files
-          loader: path => {
-            if (extname(path) === '.jsx') {
-              return 'jsx';
-            }
-          },
-      }
-    })],
+  plugins: [react(),],
   path: {'@': './dist'}
   
  
