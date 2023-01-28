edd1ba91b60f69848aa20ecd4aac8ab5403aa199
Felipe <felipecartes.u@gmail.com>
2023-01-28 06:14:11 -0300

    XDD

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,29 @@ import react from '@vitejs/plugin-react'
 
 
 
+import fs from 'fs/promises';
+
 export default defineConfig(() => ({
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
   },
   optimizeDeps: {
     esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
     },
   },
-})
-)
+}));
